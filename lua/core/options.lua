6a0804456663555b8787fc9601160665d88bf1e5
Adam Tang <adamtang2004@hotmail.com>
2026-03-01 14:24:03 +0000

    Tab settings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -5,3 +5,11 @@ opt.number = true
 
 --Numbering of lines relative to cursor
 opt.relativenumber = true
+
+--Tab settings for 4 spaces
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+
