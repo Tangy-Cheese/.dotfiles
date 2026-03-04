eb0cc2695cabaec3e74df0e23cfa2ee27637de54
Adam Tang <adamtang2004@hotmail.com>
2026-03-04 15:17:09 +0000

    Treesitter finally working updated for nvim>0.11

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,34 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,        -- REQUIRED (no lazy loading)
-    build = ":TSUpdate", -- keeps parsers synced
-  },
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+
+  config = function()
+
+    -- setup install location
+    require("nvim-treesitter").setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+    })
+
+    -- install parsers
+    require("nvim-treesitter").install({
+      "lua",
+      "python",
+      "bash",
+      "markdown",
+      "c",
+      "cpp",
+      "javascript",
+    })
+
+    -- enable treesitter highlighting
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "lua","python","bash","markdown","c","cpp","javascript" },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+
+  end,
 }
